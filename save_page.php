842d059c8e448265d69f4e1e42effa0fc57f5c0c
G-bit94 <gjuliuskelvin@gmail.com>
2024-08-07 10:10:26 -0700

    Implemented hot reload with content editable div {page}

--- save_page.php
+++ save_page.php
@@ -2,18 +2,19 @@
 
 header('Content-Type: application/json');
 
-$status = [];
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $_POST = json_decode(file_get_contents('php://input'), true);
 
-    try {
+    if (isset($_POST['content'])) {
+
         $content = $_POST['content'];
-        file_put_contents('page.php', $content);
-    } catch (Exception $e) {
-        $status["error"] = $e->getMessage();
+
+        if (file_put_contents('page.php', $content)) {
+            echo json_encode(['status' => 'success']);
+        } else {
+            echo json_encode(['status' => 'error', 'message' => 'Failed to write file.']);
+        }
     }
 
-    echo json_encode($status);
 }
